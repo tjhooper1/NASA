b115b66cafe0079ddee99584e22cb70da6219e9d
thomas hooper <tomh386@gmail.com>
2020-06-13 23:58:06 -0400

    feat: first commit

---
+++ mod.ts
@@ -0,0 +1,40 @@
+import { Application, send } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+
+const app = new Application();
+const PORT = 8000;
+
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhiteList = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png;",
+  ];
+  if (fileWhiteList.includes(filePath)) {
+    await send(ctx, filePath, { root: `${Deno.cwd()}/public` });
+  }
+});
+
+app.use((ctx) => {
+  ctx.response.body = "Hello NASA";
+});
+
+if (import.meta.main) {
+  await app.listen({
+    port: PORT,
+  });
+}
